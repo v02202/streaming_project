2d9d668682533e57ebcf31607532e052ebbd1ace
KuanYu <v0220225@gmail.com>
2023-12-12 10:59:09 +0900

    [TAS-10] Modify: admin user as foreign key

--- new_app/admin.py
+++ new_app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Users, Label, Stream, Streamer, Streamer_group, Favorite
-admin.site.register(Users)  #註冊至Administration(管理員後台)
+from .models import Label, Stream, Streamer, Streamer_group, Favorite
+#註冊至Administration(管理員後台)
 admin.site.register(Label) 
 admin.site.register(Stream)
 admin.site.register(Streamer) 
